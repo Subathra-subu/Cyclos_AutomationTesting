72c142f67b5c1e2891a6f6fece864a637ab00e60
Akksheetha <akkshee@gmail.com>
2026-05-14 22:35:18 +0530

    Added receive payment feature code

--- Cyclos/src/test/java/com/Runner/TestNGRunner.java
+++ Cyclos/src/test/java/com/Runner/TestNGRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-plugin = {"pretty", "html:reports/cucumber.html", "json:reports/cucumber.json"},
+plugin = {"pretty", "html:reports/cucumber.html", "json:reports/cucumber.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 features = "src/test/resources/features",
 glue = {"com.StepDefinitions"},
 tags="@ReceivePaymentWithValidDetails")
