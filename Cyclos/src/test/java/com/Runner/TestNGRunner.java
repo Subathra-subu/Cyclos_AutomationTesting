0dbde35ea282e2bd8330394f4a4bdcb6aa6b585b
krishnaprasath <prasathkrishna002@gmail.com>
2026-05-15 12:40:27 +0530

    updated

--- Cyclos/src/test/java/com/Runner/TestNGRunner.java
+++ Cyclos/src/test/java/com/Runner/TestNGRunner.java
@@ -1,34 +1,31 @@
 package com.Runner;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+@Test
 @CucumberOptions(
+		features = "src/test/resources/features/QR_Code.feature",
+		glue = "com.StepDefinitions",
+		plugin = {
+				"pretty",
+				"html:target/cucumber-reports.html",
+				"json:target/cucumber.json",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+		},
+		
+		monochrome=true)
 
-        features = "src/test/resources/Features/QR_Code.feature",
+public class TestNGRunner extends AbstractTestNGCucumberTests {
 
-        glue = "com.StepDefinitions",
-
-        monochrome = true,
-
-        // Disable online cucumber publish to avoid Jenkins timeout issue
-        publish = false,
-
-        plugin = {
-
-                "pretty",
-
-                "html:target/CucumberReports/Cucumber.html",
-
-                "json:target/CucumberReports/Cucumber.json",
-
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-
-                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
-        }
-)
-
-public class TestNGRunner
-        extends AbstractTestNGCucumberTests {
+	@Override
+	@DataProvider(parallel = true)
+	public Object[][] scenarios() {
+	    return super.scenarios();
+	}
 
 }
