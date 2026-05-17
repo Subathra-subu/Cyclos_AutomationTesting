1feafbe4059eb745fa62e0a1773f1e36a1b39eb8
Subathra-subu <subathrasubu2004@gmail.com>
2026-05-17 14:40:41 +0530

    DashBoard page updated

--- Cyclos/src/test/java/com/Pages/DashBoardPage.java
+++ Cyclos/src/test/java/com/Pages/DashBoardPage.java
@@ -6,7 +6,6 @@ public class DashBoardPage {
 	
 	public By dashBoard = By.xpath("//div[text()='Dashboard']");
 	
-	public By logout = By.xpath("//a[@id='logout-trigger']//icon//*[name()='svg']"); // Selector Hub
-
+	public By logout = By.cssSelector("#logout-trigger");
 
 }
