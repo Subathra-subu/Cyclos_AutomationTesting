d314101edbd75c77f40c7907ab6f1d0c3b4cdbfa
SRIRAM <wavene20@gmail.com>
2026-05-18 22:55:45 +0530

    for jenkins

--- Cyclos/src/test/java/com/Pages/InviteUserPage.java
+++ Cyclos/src/test/java/com/Pages/InviteUserPage.java
@@ -1,1 +1,13 @@
+package com.Pages;
+//
+import org.openqa.selenium.By;
 
+public class InviteUserPage {
+
+    public By marketPlace = By.xpath("//div[@class='menu-text'][normalize-space()='Marketplace']");
+    public By inviteUsers = By.xpath("//div[text()='Invite users']");
+    public By emailTextBox = By.xpath("//div[contains(@class,'input-fields')]//input[@type='text']");
+    public By sendInvitationButton = By.xpath("//span[normalize-space()='Send']");
+    public By successMessage = By.xpath("//snack-bar[contains(text(),'invitation') or contains(text(),'sent')]");
+    public By validationMessage = By.xpath("//*[contains(text(),'invalid') or contains(text(),'email')]");
+}
