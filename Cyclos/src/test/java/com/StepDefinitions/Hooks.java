b93feb2e7da481b02712ea8682f04392e1df7e0c
Subathra-subu <subathrasubu2004@gmail.com>
2026-05-18 13:17:21 +0530

    Helper,BaseAction,Configure class updated

--- Cyclos/src/test/java/com/StepDefinitions/Hooks.java
+++ Cyclos/src/test/java/com/StepDefinitions/Hooks.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
+
 import com.Utilities.HelperClass;
 
 import io.cucumber.java.After;
@@ -15,45 +16,59 @@ import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 
 public class Hooks {
-	
-	public static final Logger logger = LogManager.getLogger(Hooks.class);
-	
-	@Before
-	public static void setUp(Scenario scenario) {
-		
-		HelperClass.setupDriver();
-		
-		logger.info("Scenario started: {}",scenario.getName());
-	}
-	
-	@After
-	public static void tearDown(Scenario scenario) {
-		
-		
-		if(scenario.isFailed()) {
-			
-			File screenshotFile = ((TakesScreenshot)HelperClass.getDriver()).getScreenshotAs(OutputType.FILE);
-			
-			try {
-				File destinationFile = new File("screenshots/"+scenario.getName().replaceAll(" ","_")+".png");
-				FileUtils.copyFile(screenshotFile,destinationFile);
-				
-				byte[] screenshotBytes = ((TakesScreenshot)HelperClass.getDriver()).getScreenshotAs(OutputType.BYTES);
-				scenario.attach(screenshotBytes,"image/png","Failure Screenshot");
-				logger.error("Scenario failed: {}",scenario.getName());
-			}
-			
-			catch(Exception e) {
-				logger.error("Failed to save screenshot: {}",e.getMessage());
-			}
-		}
-		
-		else {
-			logger.info("Scenario passed : {}",scenario.getName());
-		}
-		
-		HelperClass.getDriver().quit();
-		
-	}
 
+    public static final Logger logger = LogManager.getLogger(Hooks.class);
+
+    @Before("@UserLogin")
+    public void setUplogin(Scenario scenario) {
+    	
+        logger.info("=== Scenario STARTED: {} ===", scenario.getName());
+
+        HelperClass.setupDriver();
+        HelperClass.openPage();
+        
+        
+        
+
+        logger.info("Setup complete. Browser ready.");
+    }
+
+    // AI refer 
+    @After
+  
+    public void tearDown(Scenario scenario) {
+
+        if (scenario.isFailed()) {
+
+            try {
+
+                byte[] screenshotBytes = ((TakesScreenshot) HelperClass.getDriver())
+                        .getScreenshotAs(OutputType.BYTES);
+
+                scenario.attach(screenshotBytes, "image/png", "Failure Screenshot");
+
+                File screenshotFile = ((TakesScreenshot) HelperClass.getDriver())
+                        .getScreenshotAs(OutputType.FILE);
+
+                
+                String fileName = scenario.getName().replace(" ", "_");
+
+                FileUtils.copyFile(
+                        screenshotFile,
+                        new File("screenshots/" + fileName + ".png"));
+
+                logger.error("Scenario Failed : " + scenario.getName());
+
+            } catch (Exception e) {
+
+                logger.error("Screenshot capture failed");
+            }
+
+        } else {
+
+            logger.info("Scenario Passed : " + scenario.getName());
+        }
+
+        HelperClass.tearDown();
+    }
 }
