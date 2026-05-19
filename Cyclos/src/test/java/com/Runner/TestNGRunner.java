6c29f58768134932ebd5cf39d8d7f33f77f6c14c
Akksheetha <150325137+Akksheetha@users.noreply.github.com>
2026-05-19 14:26:17 +0530

    Update TestNGRunner.java

--- Cyclos/src/test/java/com/Runner/TestNGRunner.java
+++ Cyclos/src/test/java/com/Runner/TestNGRunner.java
@@ -7,19 +7,19 @@ import io.cucumber.testng.CucumberOptions;
 
 @Test
 @CucumberOptions(
-		features = "src/test/resources/features/Advertisements.feature",
-		glue = "com.StepDefinitions",
-		plugin = {
-				"pretty",
-				"html:target/cucumber-reports.html",
-				"json:target/cucumber.json",
-				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
-		},
-	tags = "@RecievePaymentFeature"
-		monochrome=true)
+        features = "src/test/resources/features/Advertisements.feature",
+        glue = "com.StepDefinitions",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports.html",
+                "json:target/cucumber.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        },
+        tags = "@RecievePaymentFeature",
+        monochrome = true
+)
 
 public class TestNGRunner extends AbstractTestNGCucumberTests {
-
 
 }
