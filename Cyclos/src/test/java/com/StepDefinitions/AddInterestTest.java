28feb00593f4f30e2b2ed1e9dfbf6c1292c31cdf
SRIRAM K <wavene20@gmail.com>
2026-05-19 23:32:04 +0530

    PR #15 - CSV Voucher Validation and Logging Enhancement (#63)
    * Resolved merge conflict
    
    * Added ticket automation
    
    * Completed 1 senario in the in the Tickets features
    
    * feature file change
    
    * senario update
    
    * for jenkins
    
    * jenkins proplem solve
    
    * scenario-2 done
    
    * feature 2 completed
    
    * for jenkins
    
    * commanLogins
    
    * Updated feature tickets
    
    * modified ticket feature
    
    * enhance tickets and voucher feature
    
    * Refactor InviteUserPage to remove BasePage inheritance
    
    Removed inheritance from BasePage in InviteUserPage class.
    
    * Remove InviteUserPage class and its methods
    
    * Remove UserInviteSteps class and its methods
    
    * Update feature path in TestNGRunner
    
    * for jenkins
    
    * Removed generated report folders
    
    * pr#3 invite users functionality Krishna (#36)
    
    * invite users functionality version 1.0
    
    * completed Receive Qr Code Feature
    
    * Added QR code automation scenarios
    
    * reconfigured testrunner
    
    * reviewed the runner file
    
    * updated pom file
    
    * locator updated
    
    * runner class updation
    
    * updated qrcode feature
    
    * updated runner file
    
    * updated
    
    * wait time extended
    
    * working model
    
    * updated
    
    * updated feature file
    
    * Remove generated files from tracking
    
    * updated qr code feature
    
    * updated login functionality
    
    * updated common files
    
    * updated files
    
    * invite user functionaliyt
    
    * qr updated
    
    * added changes
    
    * invite user feature is added
    
    * Remove allure-results and screenshots from repo
    
    * chore: remove generated reports from repository and update automation scripts
    
    * #pr2 Qr code krishna (#32)
    
    * invite users functionality version 1.0
    
    * completed Receive Qr Code Feature
    
    * Added QR code automation scenarios
    
    * reconfigured testrunner
    
    * reviewed the runner file
    
    * updated pom file
    
    * locator updated
    
    * runner class updation
    
    * updated qrcode feature
    
    * updated runner file
    
    * updated
    
    * wait time extended
    
    * working model
    
    * updated
    
    * updated feature file
    
    * Remove generated files from tracking
    
    * updated qr code feature
    
    * updated login functionality
    
    * updated common files
    
    * updated files
    
    * invite user functionaliyt
    
    * qr updated
    
    * added changes
    
    * PR#5 - Enhancement in login feature lfile (#38)
    
    * Helper,BaseAction,Configure class updated
    
    * Added tag in feature file
    
    * Updated
    
    * Login feature - Excel data updated
    
    * Merge branch 'main' of
    https://github.com/Subathra-subu/Cyclos_AutomationTesting.git into
    USER_LOGIN_Subathra
    
    * Register feature updated
    
    * Added tag in the Login feature file
    
    ---------
    
    Co-authored-by: SRIRAM K <wavene20@gmail.com>
    
    * Delete ReqpaymentActions.java
    
    Remove ReqpaymentActions class and its methods.
    
    * Update PaymentRequestSteps.java
    
    * Update AdInterest.feature
    
    * Update MyVouchers.feature
    
    * Rename ticket feature tag from '@Tickets' to '@SRIRAM'
    
    * Update TestNGRunner to include tags for tests
    
    * Remove overridden scenarios method in TestNGRunner
    
    * enhance the feature with excel
    
    * Update feature path in TestNGRunner
    
    * enhance the feature with excel and delete scenario
    
    * enhance feature with excel integration and business directory
    
    * enchance delete
    
    * ticket feature complete
    
    * enhance the AddinterstPage
    
    * csv added
    
    * resolve conflict
    
    * Update TestNGRunner.java
    
    ---------
    
    Co-authored-by: Subathra-subu <subathrasubu2004@gmail.com>
    Co-authored-by: bkp14 <147070852+bkp14@users.noreply.github.com>
    Co-authored-by: Shobana-Viswanathan <shobanav112@gmail.com>

--- Cyclos/src/test/java/com/StepDefinitions/AddInterestTest.java
+++ Cyclos/src/test/java/com/StepDefinitions/AddInterestTest.java
@@ -14,146 +14,106 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class AddInterestTest 
-{
-	
+public class AddInterestTest {
+
 	AddInterestActions addInterestActions = new AddInterestActions();
-	
 
 	LoginAction login = new LoginAction();
-	
+
 	@Given("the user login with valid credentials")
-	public void the_user_login_with_valid_credentials() 
-	{
-	    // Write code here that turns the phrase above into concrete actions
+	public void the_user_login_with_valid_credentials() {
+		// Write code here that turns the phrase above into concrete actions
 		login.clickLoginLink();
-	    login.entervaliduserNameAndPassword();
-	    login.clickSubmitButton();
-		
+		login.entervaliduserNameAndPassword();
+		login.clickSubmitButton();
+
 	}
 
 	@Given("the user click the Marketplace menu")
 	public void the_user_click_the_marketplace_menu() {
-	    // Write code here that turns the phrase above into concrete actions
+		// Write code here that turns the phrase above into concrete actions
 		addInterestActions.clickMarketPlace();
 	}
 
-
-	
-
 	@When("the user clicks the Advertisement Interests button")
-	public void the_user_clicks_the_advertisement_interests_option() 
-	
+	public void the_user_clicks_the_advertisement_interests_option()
+
 	{
-	    // Write code here that turns the phrase above into concrete actions
+		// Write code here that turns the phrase above into concrete actions
 		addInterestActions.clickAdIntrestLink();
 	}
 
 	@When("the user clicks the Delete button")
 	public void the_user_clicks_the_delete_button() throws InterruptedException {
-	    // Write code here that turns the phrase above into concrete actions
-	    if	(addInterestActions.isVisibleResult())
-	    {
-	    	HelperClass.log.info("no row is present");
-	    }
-	    
-	    else
-	    {
-	    	addInterestActions.clickDeleteButton();
-	    }
+		// Write code here that turns the phrase above into concrete actions
+		if (addInterestActions.isVisibleResult()) {
+			HelperClass.log.info("no row is present");
+		}
+
+		else {
+			addInterestActions.clickDeleteButton();
+		}
 	}
 
 	@When("the user confirms the deletion popup")
 	public void the_user_confirms_the_deletion_popup() {
-	    // Write code here that turns the phrase above into concrete actions
-		if	(addInterestActions.isVisibleResult())
-	    {
-	    	HelperClass.log.info("no row is present");
-	    }
-	    
-	    else
-	    {
-	    	addInterestActions.clickConfirm();
-	    }
+		// Write code here that turns the phrase above into concrete actions
+		if (addInterestActions.isVisibleResult()) {
+			HelperClass.log.info("no row is present");
+		}
+
+		else {
+			addInterestActions.clickConfirm();
+		}
 	}
 
-	@Then("the user should see the success message {string}")
-	public void the_user_should_see_the_success_message(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-		if	(addInterestActions.isVisibleResult())
-	    {
-	    	
-	    }
-	    
-	    else
-	    {
-	    	addInterestActions.assertDel();
-	    }
+	@Then("the user should see the success message")
+	public void the_user_should_see_the_success_message() {
+		// Write code here that turns the phrase above into concrete actions
+		if (addInterestActions.isVisibleResult()) {
+			addInterestActions.assertNoresult();
+		}
+
+		else {
+			addInterestActions.assertDel();
+		}
 	}
+
 	
+
 	@When("the user clicks the Add New button")
 	public void the_user_clicks_the_add_new_button() {
-	    // Write code here that turns the phrase above into concrete actions
-	   addInterestActions.addNew();
+		// Write code here that turns the phrase above into concrete actions
+		addInterestActions.addNew();
 	}
 
-	
-	
-	
 	@When("the user enters all required details {string} , {string} , {string} , {string} , {string} and submit it.")
-	public void the_user_enters_all_required_details_and_submit_it(String string, String string2, String string3, String string4, String string5) throws IOException {
-	    // Write code here that turns the phrase above into concrete actions
+	public void the_user_enters_all_required_details_and_submit_it(String string, String string2, String string3,
+			String string4, String string5) throws IOException {
+		// Write code here that turns the phrase above into concrete actions
 		addInterestActions.excelData();
 	}
-	
+
 	@Then("the user enters the details without name fields and should see the {string} message")
-	public void the_user_enters_the_details_without_name_fields_and_should_see_the_message(
-	        String expectedMessage,
-	        DataTable dataTable)
-	{
-	    addInterestActions.dataTable(
-	            dataTable,
-	            expectedMessage);
-	
-	}
+	public void the_user_enters_the_details_without_name_fields_and_should_see_the_message(String expectedMessage,
+			DataTable dataTable) {
+		addInterestActions.dataTable(dataTable, expectedMessage);
 
-
-	@Then("the user should see the popup message")
-	public void the_user_should_see_the_popup_message() {
-	    // Write code here that turns the phrase above into concrete actions
-		
-		if (addInterestActions.isUniqueVisible())
-		{
-			addInterestActions.assertuniqe();
-		}
-		
-		else
-		{
-		addInterestActions.assertPopup("The ad interest was created");
-		
-		}
-		
 	}
 
 	@Then("the user should see the popup message")
 	public void the_user_should_see_the_popup_message() {
-	    // Write code here that turns the phrase above into concrete actions
-		
-		if (addInterestActions.isUniqueVisible())
-		{
+		// Write code here that turns the phrase above into concrete actions
+
+		if (addInterestActions.isUniqueVisible()) {
 			addInterestActions.assertuniqe();
 		}
-		
-		else
-		{
-		addInterestActions.assertPopup("The ad interest was created");
-		
+
+		else {
+			addInterestActions.assertPopup("The ad interest was created");
+
 		}
-		
+
 	}
-
-
-
-
 
 }
--- Cyclos/src/test/resources/features/AdInterest.feature
+++ Cyclos/src/test/resources/features/AdInterest.feature
@@ -3,8 +3,7 @@ Feature: SRIRAM_K_15/05/2026_Advertisements
   Description: Validate Advertisement Functionality
 
   Background:
-  	
-  	Given the user login with valid credentials
+    Given the user login with valid credentials
     And the user click the Marketplace menu
     And the user clicks the Advertisement Interests button
 
@@ -12,18 +11,13 @@ Feature: SRIRAM_K_15/05/2026_Advertisements
   Scenario: Delete an Advertisement Interest
     When the user clicks the Delete button
     And the user confirms the deletion popup
-    Then the user should see the success message "The item was removed"
+    Then the user should see the success message 
 
   @addNew @smoke
   Scenario: Add a New Advertisement Interest
     When the user clicks the Add New button
-<<<<<<< HEAD
-    And the user enters all required details "<name>" , "<keyWord>" , "<by>" , "min" , "max" and submit it.
-    Then the user should see the popup message "The ad interest was created"
-=======
     And the user enters all required details "<name>" , "<keyWord>" , "<by>" , "<min>" , "<max>" and submit it.
     Then the user should see the popup message
->>>>>>> 54618e4eca3f4ae10a01bdd265fadc8d6438dfa9
 
 
   @withoutName
