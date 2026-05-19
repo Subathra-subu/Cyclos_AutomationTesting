58cd98083b8f1d41108a218c73da4a7b84418d50
bkp14 <147070852+bkp14@users.noreply.github.com>
2026-05-19 23:20:07 +0530

    pr#11 Qr code krishna (#62)
    * invite users functionality version 1.0
    
    * completed Receive Qr Code Feature
    
    * Added QR code automation scenarios
    
    * reconfigured testrunner
    
    * reviewed the runner file
    
    * updated pom file
    
    * locator updated
    
    * runner class updation
    
    * updated qrcode feature
    
    * updated runner file
    
    * updated
    
    * wait time extended
    
    * working model
    
    * updated
    
    * updated feature file
    
    * Remove generated files from tracking
    
    * updated qr code feature
    
    * updated login functionality
    
    * updated common files
    
    * updated files
    
    * invite user functionaliyt
    
    * qr updated
    
    * added changes
    
    * Update TestNGRunner.java
    
    * Update TestNGRunner.java
    
    * updated
    
    * chore: force clean workspace target directory casing
    
    * updated locators
    
    * updated
    
    * locator updated
    
    * Update TestNGRunner.java

--- Cyclos/src/test/java/com/Runner/TestNGRunner.java
+++ Cyclos/src/test/java/com/Runner/TestNGRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
 @Test
 @CucumberOptions(
 
-		features = "src/test/resources/Features/req_payment.feature",
+		features = "src/test/resources/Features",
 
 
 		glue = "com.StepDefinitions",
