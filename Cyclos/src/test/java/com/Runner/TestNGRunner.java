99454b55d5bf82f784e1823b746dc765ac988361
Akksheetha <150325137+Akksheetha@users.noreply.github.com>
2026-05-19 14:18:42 +0530

    Update TestNGRunner.java

--- Cyclos/src/test/java/com/Runner/TestNGRunner.java
+++ Cyclos/src/test/java/com/Runner/TestNGRunner.java
@@ -16,6 +16,7 @@ import io.cucumber.testng.CucumberOptions;
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
 		},
+	tags = "@RecievePaymentFeature"
 		monochrome=true)
 
 public class TestNGRunner extends AbstractTestNGCucumberTests {
