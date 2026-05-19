67d455cfb17937c8c2807b960c0fab17976ac153
Subathra-subu <subathrasubu2004@gmail.com>
2026-05-19 10:43:38 +0530

    PR#5 - Enhancement in login feature lfile (#38)
    * Helper,BaseAction,Configure class updated
    
    * Added tag in feature file
    
    * Updated
    
    * Login feature - Excel data updated
    
    * Merge branch 'main' of
    https://github.com/Subathra-subu/Cyclos_AutomationTesting.git into
    USER_LOGIN_Subathra
    
    * Register feature updated
    
    * Added tag in the Login feature file
    
    ---------
    
    Co-authored-by: SRIRAM K <wavene20@gmail.com>

---
+++ Cyclos/src/test/java/com/Pages/MyAdvertisesmentsPage.java
@@ -0,0 +1,46 @@
+package com.Pages;
+
+import org.openqa.selenium.By;
+
+public class MyAdvertisesmentsPage {
+	
+		public By marketPlaceMenu = By.xpath("//div[@class='menu-text'][normalize-space()='Marketplace']");
+		
+	    public By myAdvertisements =  By.xpath("//div[normalize-space()='My advertisements']");
+
+	    public By newAdvertisementBtn = By.xpath("//div[contains(text(),'Add new')]");
+
+	    public By titleField = By.xpath("//input[@type='text']");
+
+	    public By categoryDropdown = By.xpath("//button[contains(@class,'form-control')]");
+
+	    public By categoryOption(String category) {
+
+	        return By.xpath("//a[contains(text(),'" + category + "')]");
+	    }
+
+	    public By priceField = By.xpath("//input[@type='tel']");
+
+	    public By fromDate = By.xpath("(//input[@type='date'])[1]");
+
+	    public By toDate = By.xpath("(//input[@type='date'])[2]");
+
+	    public By descriptionFrame = By.xpath("//div[contains(@class,'editor')]");
+
+	    public By uploadImageBtn = By.xpath("//input[@type='file']");
+
+	    public By saveBtn = By.xpath("//button[contains(text(),'Save')]");
+
+	    public By successMessage = By.xpath("//div[contains(@class,'notification-message')]");
+
+	    public By searchField = By.xpath("//input[@placeholder='Search']");
+
+	    public By editBtn = By.xpath("//div[contains(text(),'Edit')]");
+
+	    public By removeBtn = By.xpath("//div[contains(text(),'Remove')]");
+
+	    public By confirmPopup = By.xpath("//div[contains(text(),'Are you sure')]");
+
+	    public By confirmRemoveBtn = By.xpath("//button[contains(text(),'Remove')]");
+	}
+
