b680bb45f0bf9a1a2efff0b1e0630f4dc61770b2
krishnaprasath <prasathkrishna002@gmail.com>
2026-05-19 15:23:44 +0530

    feat: implement robust PDF file download validation for payment requests

--- Cyclos/src/test/java/com/Pages/PaymentRequestPage.java
+++ Cyclos/src/test/java/com/Pages/PaymentRequestPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 
 public class PaymentRequestPage {
 	public By Banking = By.xpath("//div[@class='menu-text'][normalize-space()='Banking']");
-	public By paymentrequest = By.cssSelector("a[class='nav-item active'] div[class='nav-item-text']");
+	public By paymentrequest = By.xpath("//div[text()='Payment requests']");
 	public By sendrequest = By.xpath("//div[normalize-space()='Send a new request']");
 	public By user = By.cssSelector("input[placeholder='Type to search']");
 	public By option = By.cssSelector("a[class='select-option autocomplete-option-0']");
@@ -15,4 +15,10 @@ public class PaymentRequestPage {
 	public  By confirm = By.xpath("//span[normalize-space()='Confirm']");
 	public By print = By.xpath("//div[contains(text(),'Print')]");
 	public By cancel = By.xpath("//div[contains(text(),'Cancel this payment request')]");
+	public By existingUser = By.xpath("//tbody//tr[1]");
+	public By paypageAssert= By.xpath("//div[text()=' Payment request details ']");
+	
+	
+	
+	
 }
