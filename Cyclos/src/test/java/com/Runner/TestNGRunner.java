06ba972b640bff65ef9d482363e1ddf35cb4306a
bkp14 <147070852+bkp14@users.noreply.github.com>
2026-05-19 10:51:59 +0530

    Update TestNGRunner.java

--- Cyclos/src/test/java/com/Runner/TestNGRunner.java
+++ Cyclos/src/test/java/com/Runner/TestNGRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 
 
-		features = "src/test/resources/Features",
+		features = "src/test/resources/Features/userInvite.feature",
 
 
         glue = "com.StepDefinitions",
