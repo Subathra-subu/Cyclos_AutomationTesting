f997a0a9f00f5c5f103d30daf599cc94f24e1223
Shobana-Viswanathan <shobanav112@gmail.com>
2026-05-20 15:26:06 +0530

    Add tags option to TestNGRunner for specific tests

--- Cyclos/src/test/java/com/Runner/TestNGRunner.java
+++ Cyclos/src/test/java/com/Runner/TestNGRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 
 		features = "src/test/resources/Features",
-       
+        tags="@BusinessDirectory_Shobana",
 
 		glue = "com.StepDefinitions",
 
