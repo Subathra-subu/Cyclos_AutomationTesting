b661cd605dcb2972098732b931b87382dfa12382
bkp14 <147070852+bkp14@users.noreply.github.com>
2026-05-20 15:55:43 +0530

    Update TestNGRunner.java

--- Cyclos/src/test/java/com/Runner/TestNGRunner.java
+++ Cyclos/src/test/java/com/Runner/TestNGRunner.java
@@ -10,9 +10,11 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 
 		features = "src/test/resources/Features",
-
+        
+        tags="@krishna",
+	
 		glue = "com.StepDefinitions",
-
+        
 		monochrome = true,
 
 		publish = false,
