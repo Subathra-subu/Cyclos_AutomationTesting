4a8a123f308c993aba3c5985cc3a4900ff490e8a
bkp14 <147070852+bkp14@users.noreply.github.com>
2026-05-20 15:50:22 +0530

    Update PaymentRequestPage.java

--- Cyclos/src/test/java/com/Pages/PaymentRequestPage.java
+++ Cyclos/src/test/java/com/Pages/PaymentRequestPage.java
@@ -1,30 +1,8 @@
-/*package com.Pages;
+package com.Pages;
 
 import org.openqa.selenium.By;
 
 public class PaymentRequestPage {
-<<<<<<< HEAD
-	public By Banking = By.xpath("//div[@class='menu-text'][normalize-space()='Banking']");
-	public By paymentrequest = By.xpath("//div[text()='Payment requests']");
-	public By sendrequest = By.xpath("//div[normalize-space()='Send a new request']");
-	public By user = By.cssSelector("input[placeholder='Type to search']");
-	public By option = By.cssSelector("a[class='select-option autocomplete-option-0']");
-	
-	public By amount = By.xpath("//input[@type='tel']");
-	public By date = By.cssSelector("input[type='date']");
-	
-	public  By confirm = By.xpath("//span[normalize-space()='Confirm']");
-	public By print = By.xpath("//div[contains(text(),'Print')]");
-	public By cancel = By.xpath("//div[contains(text(),'Cancel this payment request')]");
-	public By existingUser = By.xpath("//tbody//tr[1]");
-	public By paypageAssert= By.xpath("//div[text()=' Payment request details ']");
-	
-	
-	
-	
-}
-*/
-=======
     public By Banking = By.xpath("//div[@class='menu-text'][normalize-space()='Banking']");
     public By paymentrequest = By.xpath("//div[text()='Payment requests']");
     public By sendrequest = By.xpath("//div[normalize-space()='Send a new request']");
@@ -51,4 +29,3 @@ public class PaymentRequestPage {
     public By paypageAssert = By.xpath("//div[text()=' Payment request details ']");
     public By dateValidationError = By.xpath("//div[@class='invalid-feedback']");
 }
->>>>>>> branch 'main' of https://github.com/Subathra-subu/Cyclos_AutomationTesting.git
