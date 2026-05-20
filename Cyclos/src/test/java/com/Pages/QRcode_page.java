b13238f84d6da43cad1809c911478b4800fc1b1b
bkp14 <147070852+bkp14@users.noreply.github.com>
2026-05-20 16:58:48 +0530

    Update QRcode locator for image element

--- Cyclos/src/test/java/com/Pages/QRcode_page.java
+++ Cyclos/src/test/java/com/Pages/QRcode_page.java
@@ -10,7 +10,7 @@ public class QRcode_page  {
 	public By QRcode = By.xpath("//div[text()='Receive QR-code']");
 	public By Amount = By.xpath("//input[contains(@placeholder,'0')]");
 	public By Generate = By.xpath("//span[normalize-space()='Generate QR code']");
-	public By Qrcode = By.xpath("//img[@class='mb-3']");
+	public By Qrcode = By.xpath("//receive-qr-payment-step-pending/descendant::img[1]");
 	public By errormsg = By.xpath("//div[@class='invalid-feedback']");
 	public By new_generate = By.xpath("//span[text()='Generate new QR code']");
 
