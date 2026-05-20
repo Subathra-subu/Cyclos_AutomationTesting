6037d206ef27d02cc3d9ff413362088784e27619
bkp14 <147070852+bkp14@users.noreply.github.com>
2026-05-20 17:08:56 +0530

    Update error message locator in QRcode_page.java

--- Cyclos/src/test/java/com/Pages/QRcode_page.java
+++ Cyclos/src/test/java/com/Pages/QRcode_page.java
@@ -11,7 +11,7 @@ public class QRcode_page  {
 	public By Amount = By.xpath("//input[contains(@placeholder,'0')]");
 	public By Generate = By.xpath("//span[normalize-space()='Generate QR code']");
 	public By Qrcode = By.xpath("//receive-qr-payment-step-pending/descendant::img[1]");
-	public By errormsg = By.xpath("//div[@class='invalid-feedback']");
+	public By errormsg = By.xpath("//field-errors/descendant::div");
 	public By new_generate = By.xpath("//span[text()='Generate new QR code']");
 
 
