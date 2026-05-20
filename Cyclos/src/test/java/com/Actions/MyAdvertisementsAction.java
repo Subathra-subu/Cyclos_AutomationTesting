477e93ec2e6e96c3b8fdb3e45f6de86344f05ff0
Akksheetha <150325137+Akksheetha@users.noreply.github.com>
2026-05-20 15:43:55 +0530

    Update MyAdvertisementsAction.java

--- Cyclos/src/test/java/com/Actions/MyAdvertisementsAction.java
+++ Cyclos/src/test/java/com/Actions/MyAdvertisementsAction.java
@@ -1,232 +1,232 @@
-package com.Actions;
+// package com.Actions;
 
-import org.openqa.selenium.By;
-import org.testng.Assert;
+// import org.openqa.selenium.By;
+// import org.testng.Assert;
 
-import com.Pages.MyAdvertisesmentsPage;
-import com.StepDefinitions.Hooks;
+// import com.Pages.MyAdvertisesmentsPage;
+// import com.StepDefinitions.Hooks;
 
-public class MyAdvertisementsAction extends BaseAction {
+// public class MyAdvertisementsAction extends BaseAction {
 	
-	MyAdvertisesmentsPage advertisementPage = new MyAdvertisesmentsPage();
+// 	MyAdvertisesmentsPage advertisementPage = new MyAdvertisesmentsPage();
 	
-    public void clickMarketPlaceMenu() {
+//     public void clickMarketPlaceMenu() {
 
-        try {
-            waitForVisibility(advertisementPage.marketPlaceMenu);
+//         try {
+//             waitForVisibility(advertisementPage.marketPlaceMenu);
 
-            jsClick(advertisementPage.marketPlaceMenu);
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to click marketplace menu", e);
-        }
-    }
+//             jsClick(advertisementPage.marketPlaceMenu);
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to click marketplace menu", e);
+//         }
+//     }
 
-    public void clickMyAdvertisements() {
+//     public void clickMyAdvertisements() {
 
-        try {
-            waitForVisibility(advertisementPage.myAdvertisements);
+//         try {
+//             waitForVisibility(advertisementPage.myAdvertisements);
 
-            jsClick(advertisementPage.myAdvertisements);
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to click my advertisements", e);
-        }
+//             jsClick(advertisementPage.myAdvertisements);
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to click my advertisements", e);
+//         }
 
-    }
+//     }
 
-    public void clickNewAdvertisement() {
+//     public void clickNewAdvertisement() {
 
-        try {
-            click(advertisementPage.newAdvertisementBtn);
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to click new advertisement button", e);
-        }
-    }
+//         try {
+//             click(advertisementPage.newAdvertisementBtn);
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to click new advertisement button", e);
+//         }
+//     }
 
-    public void enterTitle(String title) {
+//     public void enterTitle(String title) {
 
-        try {
-            sendKeys(advertisementPage.titleField, title);
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to enter title", e);
-        }
-    }
+//         try {
+//             sendKeys(advertisementPage.titleField, title);
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to enter title", e);
+//         }
+//     }
 
-    public void selectCategory(String category) {
+//     public void selectCategory(String category) {
 
-        try {
-            click(advertisementPage.categoryDropdown);
+//         try {
+//             click(advertisementPage.categoryDropdown);
 
-            By option = advertisementPage.categoryOption(category);
+//             By option = advertisementPage.categoryOption(category);
 
-            waitForVisibility(option);
+//             waitForVisibility(option);
 
-            click(option);
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to select category", e);
-        }
-    }
+//             click(option);
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to select category", e);
+//         }
+//     }
 
-    public void enterPrice(String price) {
+//     public void enterPrice(String price) {
 
-        try {
-            sendKeys(advertisementPage.priceField, price);
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to enter price", e);
-        }
-    }
+//         try {
+//             sendKeys(advertisementPage.priceField, price);
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to enter price", e);
+//         }
+//     }
 
-    public void selectFromDate(String date) {
+//     public void selectFromDate(String date) {
 
-        try {
-            sendKeys(advertisementPage.fromDate, date);
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to select from date", e);
-        }
-    }
+//         try {
+//             sendKeys(advertisementPage.fromDate, date);
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to select from date", e);
+//         }
+//     }
 
-    public void selectToDate(String date) {
+//     public void selectToDate(String date) {
 
-        try {
-            sendKeys(advertisementPage.toDate, date);
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to select to date", e);
-        }
-    }
+//         try {
+//             sendKeys(advertisementPage.toDate, date);
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to select to date", e);
+//         }
+//     }
 
-    public void enterDescription(String description) {
+//     public void enterDescription(String description) {
 
-        try {
-            waitForVisibility(advertisementPage.descriptionFrame);
+//         try {
+//             waitForVisibility(advertisementPage.descriptionFrame);
 
-            sendKeys(advertisementPage.descriptionFrame, description);
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to enter description", e);
-        }
-    }
+//             sendKeys(advertisementPage.descriptionFrame, description);
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to enter description", e);
+//         }
+//     }
 
-    public void clickSave() {
+//     public void clickSave() {
 
-        try {
-            waitForVisibility(advertisementPage.saveBtn);
+//         try {
+//             waitForVisibility(advertisementPage.saveBtn);
 
-            click(advertisementPage.saveBtn);
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to click save button", e);
-        }
-    }
+//             click(advertisementPage.saveBtn);
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to click save button", e);
+//         }
+//     }
 
-    public String validateMessage() {
+//     public String validateMessage() {
 
-        try {
-            return getText(advertisementPage.Message);
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to validate message", e);
-        }
+//         try {
+//             return getText(advertisementPage.Message);
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to validate message", e);
+//         }
 
-        return null;
-    }
+//         return null;
+//     }
 
-    public void assertSuccessfullMessage() {
+//     public void assertSuccessfullMessage() {
 
-        try {
-            isDisplayed(advertisementPage.Message);
+//         try {
+//             isDisplayed(advertisementPage.Message);
 
-            Assert.assertTrue(validateMessage().contains("was saved"));
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to assert successful message", e);
-        }
-    }
+//             Assert.assertTrue(validateMessage().contains("was saved"));
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to assert successful message", e);
+//         }
+//     }
 
-    public void searchAdvertisement(String title) {
+//     public void searchAdvertisement(String title) {
 
-        try {
-            sendKeys(advertisementPage.searchField, title);
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to search advertisement", e);
-        }
-    }
+//         try {
+//             sendKeys(advertisementPage.searchField, title);
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to search advertisement", e);
+//         }
+//     }
 
-    public void clickItem() {
+//     public void clickItem() {
 
-        try {
-        	waitForVisibility(advertisementPage.item);
-            click(advertisementPage.item);
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to click item", e);
-        }
-    }
+//         try {
+//         	waitForVisibility(advertisementPage.item);
+//             click(advertisementPage.item);
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to click item", e);
+//         }
+//     }
 
-    public void clickEditAdvertisement() {
+//     public void clickEditAdvertisement() {
 
-        try {
-            click(advertisementPage.editBtn);
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to click edit button", e);
-        }
-    }
+//         try {
+//             click(advertisementPage.editBtn);
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to click edit button", e);
+//         }
+//     }
 
-    public void clickRemoveAdvertisement() {
+//     public void clickRemoveAdvertisement() {
 
-        try {
-            click(advertisementPage.removeBtn);
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to click remove button", e);
-        }
-    }
+//         try {
+//             click(advertisementPage.removeBtn);
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to click remove button", e);
+//         }
+//     }
 
-    public String validateRemovePopup() {
+//     public String validateRemovePopup() {
 
-        try {
-            return getText(advertisementPage.confirmPopup);
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to validate remove popup", e);
-        }
+//         try {
+//             return getText(advertisementPage.confirmPopup);
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to validate remove popup", e);
+//         }
 
-        return null;
-    }
+//         return null;
+//     }
 
-    public void confirmRemoveAdvertisement() {
+//     public void confirmRemoveAdvertisement() {
 
-        try {
-            click(advertisementPage.confirmRemoveBtn);
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to confirm remove advertisement", e);
-        }
-    }
+//         try {
+//             click(advertisementPage.confirmRemoveBtn);
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to confirm remove advertisement", e);
+//         }
+//     }
 
-    public void assertRemovalMessage() {
+//     public void assertRemovalMessage() {
 
-        try {
-            isDisplayed(advertisementPage.Message);
+//         try {
+//             isDisplayed(advertisementPage.Message);
 
-            Assert.assertTrue(validateMessage().contains("was removed"));
-        } 
-        catch (Exception e) {
-            Hooks.logger.error("Unable to assert removal message", e);
-        }
-    }
+//             Assert.assertTrue(validateMessage().contains("was removed"));
+//         } 
+//         catch (Exception e) {
+//             Hooks.logger.error("Unable to assert removal message", e);
+//         }
+//     }
 
-   }
+//    }
 
-   }
+//    }
 
 
 
