51aba4558757ff8c6f92146e1551fd54c664b7ed
SRIRAM K <wavene20@gmail.com>
2026-05-20 15:18:31 +0530

    # PR #15 - HTML Report Configuration & Runner Updates (#72)
    * Resolved merge conflict
    
    * Added ticket automation
    
    * Completed 1 senario in the in the Tickets features
    
    * feature file change
    
    * senario update
    
    * for jenkins
    
    * jenkins proplem solve
    
    * scenario-2 done
    
    * feature 2 completed
    
    * for jenkins
    
    * commanLogins
    
    * Updated feature tickets
    
    * modified ticket feature
    
    * enhance tickets and voucher feature
    
    * Refactor InviteUserPage to remove BasePage inheritance
    
    Removed inheritance from BasePage in InviteUserPage class.
    
    * Remove InviteUserPage class and its methods
    
    * Remove UserInviteSteps class and its methods
    
    * Update feature path in TestNGRunner
    
    * for jenkins
    
    * Removed generated report folders
    
    * pr#3 invite users functionality Krishna (#36)
    
    * invite users functionality version 1.0
    
    * completed Receive Qr Code Feature
    
    * Added QR code automation scenarios
    
    * reconfigured testrunner
    
    * reviewed the runner file
    
    * updated pom file
    
    * locator updated
    
    * runner class updation
    
    * updated qrcode feature
    
    * updated runner file
    
    * updated
    
    * wait time extended
    
    * working model
    
    * updated
    
    * updated feature file
    
    * Remove generated files from tracking
    
    * updated qr code feature
    
    * updated login functionality
    
    * updated common files
    
    * updated files
    
    * invite user functionaliyt
    
    * qr updated
    
    * added changes
    
    * invite user feature is added
    
    * Remove allure-results and screenshots from repo
    
    * chore: remove generated reports from repository and update automation scripts
    
    * #pr2 Qr code krishna (#32)
    
    * invite users functionality version 1.0
    
    * completed Receive Qr Code Feature
    
    * Added QR code automation scenarios
    
    * reconfigured testrunner
    
    * reviewed the runner file
    
    * updated pom file
    
    * locator updated
    
    * runner class updation
    
    * updated qrcode feature
    
    * updated runner file
    
    * updated
    
    * wait time extended
    
    * working model
    
    * updated
    
    * updated feature file
    
    * Remove generated files from tracking
    
    * updated qr code feature
    
    * updated login functionality
    
    * updated common files
    
    * updated files
    
    * invite user functionaliyt
    
    * qr updated
    
    * added changes
    
    * PR#5 - Enhancement in login feature lfile (#38)
    
    * Helper,BaseAction,Configure class updated
    
    * Added tag in feature file
    
    * Updated
    
    * Login feature - Excel data updated
    
    * Merge branch 'main' of
    https://github.com/Subathra-subu/Cyclos_AutomationTesting.git into
    USER_LOGIN_Subathra
    
    * Register feature updated
    
    * Added tag in the Login feature file
    
    ---------
    
    Co-authored-by: SRIRAM K <wavene20@gmail.com>
    
    * Delete ReqpaymentActions.java
    
    Remove ReqpaymentActions class and its methods.
    
    * Update PaymentRequestSteps.java
    
    * Update AdInterest.feature
    
    * Update MyVouchers.feature
    
    * Rename ticket feature tag from '@Tickets' to '@SRIRAM'
    
    * Update TestNGRunner to include tags for tests
    
    * Remove overridden scenarios method in TestNGRunner
    
    * enhance the feature with excel
    
    * Update feature path in TestNGRunner
    
    * enhance the feature with excel and delete scenario
    
    * enhance feature with excel integration and business directory
    
    * enchance delete
    
    * ticket feature complete
    
    * enhance the AddinterstPage
    
    * Update TestNGRunner.java
    
    * csv added
    
    * resolve conflict
    
    * resolve conflict
    
    * document added
    
    * document added
    
    * resolve conflict
    
    * resolve conflict again
    
    * HTML report
    
    ---------
    
    Co-authored-by: Subathra-subu <subathrasubu2004@gmail.com>
    Co-authored-by: bkp14 <147070852+bkp14@users.noreply.github.com>
    Co-authored-by: Shobana-Viswanathan <shobanav112@gmail.com>

--- Cyclos/src/test/java/com/Utilities/CSVUtility.java
+++ Cyclos/src/test/java/com/Utilities/CSVUtility.java
@@ -7,60 +7,37 @@ import java.util.List;
 
 public class CSVUtility {
 
-<<<<<<< HEAD
-    public static List<String> getVoucherCodes(String filePath) {
-
-        List<String> voucherCodes =
-                new ArrayList<>();
-=======
 	public static List<String> getVoucherCodes(String filePath) {
 
 		List<String> voucherCodes = new ArrayList<>();
->>>>>>> 3763e922d7bc4f5935daf9f118dd907d7cf6fb74
 
-		try {
 
-<<<<<<< HEAD
-            BufferedReader br =
-                    new BufferedReader(
-                            new FileReader(filePath));
-=======
+        try {
+
 			BufferedReader br = new BufferedReader(new FileReader(filePath));
->>>>>>> 3763e922d7bc4f5935daf9f118dd907d7cf6fb74
 
-			br.readLine();
 
-			String line;
+            // Skip header row
+            br.readLine();
 
-			while ((line = br.readLine()) != null) {
+            String line;
 
-				String[] data = line.split(",");
+            while ((line = br.readLine()) != null) {
 
-<<<<<<< HEAD
-                voucherCodes.add(data[0].trim());
-            }
-=======
+                String[] data = line.split(",");
+
 				voucherCodes.add(data[0].trim());
 			}
->>>>>>> 3763e922d7bc4f5935daf9f118dd907d7cf6fb74
 
-			br.close();
 
-<<<<<<< HEAD
-        } catch (Exception e) {
+            br.close();
+
+		} catch (Exception e) {
 
             e.printStackTrace();
         }
 
-        return voucherCodes;
-    }
-=======
-		} catch (Exception e) {
-
-			e.printStackTrace();
-		}
-
 		return voucherCodes;
 	}
->>>>>>> 3763e922d7bc4f5935daf9f118dd907d7cf6fb74
+
 }
