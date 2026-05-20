b1da09046e4f94c8eb94441823b4b6352eec778f
bkp14 <147070852+bkp14@users.noreply.github.com>
2026-05-20 15:52:54 +0530

    Refactor getVoucherCodes method for clarity

--- Cyclos/src/test/java/com/Utilities/CSVUtility.java
+++ Cyclos/src/test/java/com/Utilities/CSVUtility.java
@@ -7,46 +7,30 @@ import java.util.List;
 
 public class CSVUtility {
 
-<<<<<<< HEAD
     public static List<String> getVoucherCodes(String filePath) {
 
-        List<String> voucherCodes =
-                new ArrayList<>();
-=======
-	public static List<String> getVoucherCodes(String filePath) {
+        List<String> voucherCodes = new ArrayList<>();
 
-		List<String> voucherCodes = new ArrayList<>();
->>>>>>> 3763e922d7bc4f5935daf9f118dd907d7cf6fb74
+        try {
 
-		try {
-
-<<<<<<< HEAD
             BufferedReader br =
                     new BufferedReader(
                             new FileReader(filePath));
-=======
-			BufferedReader br = new BufferedReader(new FileReader(filePath));
->>>>>>> 3763e922d7bc4f5935daf9f118dd907d7cf6fb74
 
-			br.readLine();
+            
+            br.readLine();
 
-			String line;
+            String line;
 
-			while ((line = br.readLine()) != null) {
+            while ((line = br.readLine()) != null) {
 
-				String[] data = line.split(",");
+                String[] data = line.split(",");
 
-<<<<<<< HEAD
                 voucherCodes.add(data[0].trim());
             }
-=======
-				voucherCodes.add(data[0].trim());
-			}
->>>>>>> 3763e922d7bc4f5935daf9f118dd907d7cf6fb74
 
-			br.close();
+            br.close();
 
-<<<<<<< HEAD
         } catch (Exception e) {
 
             e.printStackTrace();
@@ -54,13 +38,4 @@ public class CSVUtility {
 
         return voucherCodes;
     }
-=======
-		} catch (Exception e) {
-
-			e.printStackTrace();
-		}
-
-		return voucherCodes;
-	}
->>>>>>> 3763e922d7bc4f5935daf9f118dd907d7cf6fb74
 }
