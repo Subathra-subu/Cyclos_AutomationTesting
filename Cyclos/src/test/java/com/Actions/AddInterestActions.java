b2662290d5fd8777041aef535375e05662f46b1d
bkp14 <147070852+bkp14@users.noreply.github.com>
2026-05-20 09:25:56 +0530

    Update AddInterestActions.java

--- Cyclos/src/test/java/com/Actions/AddInterestActions.java
+++ Cyclos/src/test/java/com/Actions/AddInterestActions.java
@@ -79,8 +79,7 @@ public class AddInterestActions extends BaseAction {
 			throw e;
 		}
 	}
-<<<<<<< HEAD
-=======
+
 	
 	public void assertuniqe() {
 
@@ -103,7 +102,6 @@ public class AddInterestActions extends BaseAction {
 			throw e;
 		}
 	}
->>>>>>> 54618e4eca3f4ae10a01bdd265fadc8d6438dfa9
 
 
 	public void clickMarketPlace() {
