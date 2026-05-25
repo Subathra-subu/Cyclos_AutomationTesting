fd50e0d1ad85f85f093f7c3a7585040a9b6a716b
SRIRAM <wavene20@gmail.com>
2026-05-25 13:48:52 +0530

    conflict

--- Cyclos/src/test/java/com/Runner/TestNGRunner.java
+++ Cyclos/src/test/java/com/Runner/TestNGRunner.java
@@ -7,18 +7,30 @@ import io.cucumber.testng.CucumberOptions;
 
 
 @CucumberOptions(
-        features  = "src/test/resources/Features",
-        glue      = "com.StepDefinitions",
-        monochrome = true,
-        publish   = false,
-        tags      = "@SRIRAM",
-        plugin    = {
-                "pretty",
-                "html:target/CucumberReports/Cucumber.html",
-                "json:target/CucumberReports/Cucumber.json",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
-        })
+
+
+		features = "src/test/resources/Features",
+
+		glue = "com.StepDefinitions",
+
+		monochrome = true,
+
+		publish = false,
+
+		tags = "@SRIRAM",
+	
+		plugin = {
+
+				"pretty",
+
+				"html:target/CucumberReports/Cucumber.html",
+
+				"json:target/CucumberReports/Cucumber.json",
+
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+
+				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" })
+
 public class TestNGRunner extends AbstractTestNGCucumberTests {
 
     
