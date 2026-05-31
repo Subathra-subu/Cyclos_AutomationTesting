44a5b8a86b9343a89ca205e0db5ff034fcd53fcf
Akksheetha <akkshee@gmail.com>
2026-05-31 13:16:51 +0530

    New scenario added

--- Cyclos/src/test/java/com/Runner/TestNGRunner.java
+++ Cyclos/src/test/java/com/Runner/TestNGRunner.java
@@ -12,8 +12,6 @@ import io.cucumber.testng.CucumberOptions;
 		features = "src/test/resources/Features",
 
 		glue = "com.StepDefinitions",
-		
-		tags = "@MainPageListing",
 
 		monochrome = true,
 
