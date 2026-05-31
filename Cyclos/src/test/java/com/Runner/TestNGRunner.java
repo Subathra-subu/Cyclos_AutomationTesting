9987fa79f1ba4449fc209a5bca13362fc366d007
Subathra-subu <subathrasubu2004@gmail.com>
2026-05-31 17:34:44 +0530

    Update Cucumber feature path in TestNGRunner

--- Cyclos/src/test/java/com/Runner/TestNGRunner.java
+++ Cyclos/src/test/java/com/Runner/TestNGRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 
 
-		features = "src/test/resources/Features/Messages.feature",
+		features = "src/test/resources/Features",
 
 		glue = "com.StepDefinitions",
 
