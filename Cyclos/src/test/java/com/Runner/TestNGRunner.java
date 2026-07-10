12bfa088ceaf25659eda226ada0097c5af438ec8
bkp14 <147070852+bkp14@users.noreply.github.com>
2026-07-10 09:11:12 +0530

    Update TestNGRunner to use @krishna tag

--- Cyclos/src/test/java/com/Runner/TestNGRunner.java
+++ Cyclos/src/test/java/com/Runner/TestNGRunner.java
@@ -12,7 +12,7 @@ import io.cucumber.testng.CucumberOptions;
 		features = "src/test/resources/Features",
 
 
-		glue = "com.StepDefinitions", tags="@validpaymentrequest",
+		glue = "com.StepDefinitions", tags="@krishna",
 
 		monochrome = true,
 
