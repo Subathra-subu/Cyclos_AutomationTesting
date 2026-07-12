b336274397086cc8dce0576247fe170f7f2f848d
Akksheetha <akkshee@gmail.com>
2026-07-12 15:28:49 +0530

    Added Unread Notification scenario

--- Cyclos/src/test/java/com/Pages/NotificationsPage.java
+++ Cyclos/src/test/java/com/Pages/NotificationsPage.java
@@ -7,4 +7,6 @@ public class NotificationsPage {
 	public By notificationsIcon = By.id("notifications-link");
     public By notificationsActiveMenu = By.xpath("//a[@id='notifications-link' and contains(@class,'active')]/parent::div");
     public By unreadOnlyCheckbox = By.xpath("//label[normalize-space()='Unread only']");
+    public By unreadCheckbox = By.xpath("//input[@type='checkbox']");
+    public By notificationRows = By.xpath("//table/tbody/tr");
 }
